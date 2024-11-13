3bd061710d7a293889bf8ad1c9d6267fcc9311a1
Rupeshpharande <pharanderupesh@gmail.com>
2024-11-14 01:19:24 +0530

    Add project files.

---
+++ JaggedArraydemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArraydemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[][] jarray = new int[4][];
+
+           jarray[0] = new int[3] {10,20,30};
+          jarray[1]=new int[4] { 40,50,60,70};
+           jarray[2]=new int[5] { 80,90,100,110,120};
+           jarray[3]=new int[2] {130,140};
+
+            //    for (int i = 0; i < jarray.Length; i++)
+            //    {
+            //        for(int j = 0;j< jarray[i].Length;j++)
+            //        {
+            //            Console.Write($"jarray[{i}],[{j}]= {jarray[i][j]} ");
+            //        }
+            //        Console.WriteLine();
+            //    }
+
+            foreach (int[] p in jarray) 
+                {
+                   foreach(int i in p)
+                   {
+
+                    Console.Write(i+" ");
+                   }
+                   Console.WriteLine();
+             }
+        }
+    }
+}
---
+++ JaggedArraydemo/UserInput.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArraydemo
+{
+    internal class UserInput
+    {
+        //static void Main(string[] args)
+        //{
+        //    int[][] jarr = new int[4][];
+
+        //    jarr[0] = new int[2] ;
+        //    jarr[1] = new int[3];
+        //    jarr[2] = new int[4];
+        //    jarr[3] = new int[1];
+
+        //    for (int i = 0; i < jarr.Length; i++)
+        //    {
+        //        for (int j = 0; j < jarr[i].Length; j++)
+        //        {
+                   
+        //            jarr[i][j]=Convert.ToInt32(Console.ReadLine());
+                    
+
+                   
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    for (int i = 0; i < jarr.Length; i++)
+        //    {
+        //        {
+        //            for (int j = 0; j < jarr[i].Length; j++)
+        //            {
+        //                Console.Write($"jarr[{i}][{j}]={jarr[i][j]} ");
+        //            }
+        //            Console.WriteLine();
+        //        }
+
+
+        //    }
+
+        //}
+    }
+}
